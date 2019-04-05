23023550c80a0a26dec9b912e15774bee2e47083
prajwollamichhane11 <prajwollamichhane11@gmail.com>
2019-04-05 08:18:05 +0545

    The Bubble Sort

---
+++ Sorting/BubbleSort.py
@@ -0,0 +1,13 @@
+print ('Enter numbers to implement sorting as: N N N N N')
+arr = list(map(int,input().split()))
+
+length = len(arr)
+
+for i in range(length):
+    for j in range(0,length-i-1):
+        if arr[j] > arr[j+1]:
+            temp = arr[j]
+            arr[j] = arr[j+1]
+            arr[j+1] = temp
+print('The Sorted numbers are:')
+print(arr)
