835602c1ab2ed7ed5a2831b992e0376303293311
prajwollamichhane11 <prajwollamichhane11@gmail.com>
2019-04-08 08:36:33 +0545

    Insertion Sort

---
+++ Sorting/InsertionSort.py
@@ -0,0 +1,11 @@
+A = list(map(int,input().split()))
+
+for j in range (2,len(A)):
+    key = A[j]
+    i = j-1
+    while i>0 and A[i] > key:
+        A[i+1] = A[i]
+        i = i-1
+    A[i+1] = key
+print(A)
+ 
