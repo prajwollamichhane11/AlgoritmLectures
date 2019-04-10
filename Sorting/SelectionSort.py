a508a68d40f278cc5c9d2b08083ca2fe692e636e
prajwollamichhane11 <prajwollamichhane11@gmail.com>
2019-04-10 10:17:27 +0545

    Corrected

--- Sorting/SelectionSort.py
+++ Sorting/SelectionSort.py
@@ -4,17 +4,15 @@ arr = []
 print ('Enter numbers to implement sorting as: N N N N N')
 arr = list(map(int,input().split()))
 
-length = len(arr)
-
 start = time.time()
-for i in range(length):
-    min = arr[i]
-    for j in range(i+1,length):
-        if arr[j] < min :
-            temp = min
-            min = arr[j]
-            arr[j] = temp
-    arr[i] = min
+for i in range(len(arr)-1):
+    min = i
+    for j in range(i+1,len(arr)):
+        if arr[j] < arr[min] :
+            min = j
+    temp = arr[i]
+    arr[i] = arr[min]
+    arr[min] = temp
 end = time.time()
 
 print (arr)
