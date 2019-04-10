901e7706aa7636dcf4fc526e866bcbe1c6a8cb92
prajwollamichhane11 <prajwollamichhane11@gmail.com>
2019-04-10 10:39:32 +0545

    Corrected

--- Sorting/InsertionSort.py
+++ Sorting/InsertionSort.py
@@ -3,10 +3,10 @@ import time
 A = list(map(int,input().split()))
 
 start = time.time()
-for j in range (2,len(A)):
+for j in range (1,len(A)):
     key = A[j]
     i = j-1
-    while i>0 and A[i] > key:
+    while i>=0 and A[i] > key:
         A[i+1] = A[i]
         i = i-1
     A[i+1] = key
