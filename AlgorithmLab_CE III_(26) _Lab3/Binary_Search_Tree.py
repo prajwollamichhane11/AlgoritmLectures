a1817a5fc83e004fd1c735d357b74f03f0da5997
prajwollamichhane11 <prajwollamichhane11@gmail.com>
2019-05-20 10:11:13 +0545

    Algorithm Lab 3

--- AlgorithmLab_CE III_(26) _Lab3/Binary_Search_Tree.py
+++ AlgorithmLab_CE III_(26) _Lab3/Binary_Search_Tree.py
@@ -1,81 +1,105 @@
 #Binary Search Tree in Python
+class BST:
+	def __init__(self):
+		self._size = 0
+		self._root = None #type bstnode
 
-class Binary_Search_Tree:
+	class _BSTNode:
+		def __init__(self, key, value):
+			self.key = key
+			self.value = value
+			self.left = None
+			self.right = None
 
-    def __init__(self):
-        self._size = 0
-        self._root = None
+	def addNode(self, key, value):
+		z = self._BSTNode(key, value)
+		y =None
+		x = self._root
+		while (x != None):
+			y = x
+			if (key < x.key):
+				x = x.left
+			else:
+				x = x.right
+		if (y == None):
+			self._root = z
+		elif (z.key < y.key):
+			y.left = z
+		else:
+			y.right = z
+		self._size += 1
 
-    class _BSTNode:
-        def __init__(self,key,value):
-            self.key = key
-            self.value = value
-            self.right = None
-            self.left = None
+	def isEmpty(self):
+		return self._size == 0
 
-    def add_node(self,key,value):
-        z = self._BSTNode(key,value)
-        y = None
-        x = self._root
-        while (x != None):
-            y = x
-            if(key < x.key) :
-                x = x.left
-            else:
-                x = x.right
-        if (y == None):
-            self._root = z
-        elif (z.key < y.key):
-            y.left = z
-        else:
-            y.right = z
-        self._size += 1
+	def size(self):
+		return self._size
 
-    def is_empty(self):
-        return self._size == 0
+	def inOrderTraverse(self):
+		nodes = []
+		self._inOrderTraverse(self._root, nodes)
+		return nodes
 
-    def size(self):
-        return self._size
+	def _inOrderTraverse(self, subtree, nodes):
+		if(subtree):
+			self._inOrderTraverse(subtree.left, nodes)
+			nodes.append(subtree.key)
+			self._inOrderTraverse(subtree.right, nodes)
 
-    def inorder_walk(self):
-        nodes = []
-        self._inorder_walk(self._root,nodes)
-        return nodes
+	def preOrderTraverse(self):
+		nodes = []
+		self._preOrderTraverse(self._root, nodes)
+		return nodes
 
-    def _inorder_walk(self,subtree,nodes):
-        if subtree:
-            self._inorder_walk(subtree.left,nodes)
-            nodes.append(subtree.key)
-            self._inorder_walk(subtree.right,nodes)
+	def _preOrderTraverse(self, subtree, nodes):
+		if(subtree):
+			nodes.append(subtree.key)
+			self._preOrderTraverse(subtree.left, nodes)
+			self._preOrderTraverse(subtree.right, nodes)
 
-    def preorder_walk(self):
-        nodes = []
-        self._preorderwalk(self._root,nodes)
-        return nodes
+	def postOrderTraverse(self):
+		nodes = []
+		self._postOrderTraverse(self._root, nodes)
+		return nodes
 
-    def _preorderwalk(self,subtree,nodes):
-        if subtree:
-            nodes.append(subtree.key)
-            self._preorderwalk(subtree.left,nodes)
-            self._preorderwalk(subtree.right,nodes)
+	def _postOrderTraverse(self, subtree, nodes):
+		if(subtree):
+			self._preOrderTraverse(subtree.left, nodes)
+			self._preOrderTraverse(subtree.right, nodes)
+			nodes.append(subtree.key)
 
-    def postorder_walk(self):
-        nodes = []
-        self._postorderwalk(self._root,nodes)
-        return nodes
+	def searchForNode(self, key):
+		found = []
+		self._searchForNode(self._root, key, found)
+		return found
 
-    def _postorderwalk(self,subtree,nodes):
-        if subtree:
-            self._postorderwalk(subtree.left,nodes)
-            self._postorderwalk(subtree.right,nodes)
-            nodes.append(subtree.key)
+	def _searchForNode(self, subtree, key, found):
+		if(subtree):
+			if(key == subtree.key):
+				found.append(1)
+			elif(key < subtree.key):
+				self._searchForNode(subtree.left, key, found)
+			elif(key > subtree.key):
+				self._searchForNode(subtree.right, key, found)
 
+	def searchSmallestKey(self):
+		nodes = []
+		self._searchSmallestKey(self._root, nodes)
+		return nodes
 
-    def minimumValue(key):
-        current = key
+	def _searchSmallestKey(self, subtree, nodes):
+		if(subtree):
+			if(subtree.left == None):
+				nodes.append(subtree.key)
+			self._searchSmallestKey(subtree.left, nodes)
 
-        # loop down to find the leftmost leaf
-        while(current.left is not None):
-            current = current.left
+	def searchLargestKey(self):
+		nodes = []
+		self._searchLargestKey(self._root, nodes)
+		return nodes
 
-        return current  
+	def _searchLargestKey(self, subtree, nodes):
+		if(subtree):
+			if(subtree.right == None):
+				nodes.append(subtree.key)
+			self._findLargestKey(subtree.right, nodes)
