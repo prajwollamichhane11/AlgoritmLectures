2d189ed1f5315b0400b8c87c177dad9f5a10d327
prajwollamichhane11 <prajwollamichhane11@gmail.com>
2019-06-09 21:22:00 +0545

    dasdasda

--- AlgorithmLab_CE III_(26) _Lab4/activityselectorrecursive.py
+++ AlgorithmLab_CE III_(26) _Lab4/activityselectorrecursive.py
@@ -1,25 +1,28 @@
-act = [a[m]]
-
-def recu_act_sel(s,f,k,n):
+def recu_act_sel(s,f,k,n,act):
     m = k+1
-    while m<=n and s[m]<f[k]:
+    while m<n and s[m]<f[k] and k>0:
         m = m+1
     if m<=n:
-        return act.append(recu_act_sel(s,f,m,n))
-    else:
+        act.append(m)
+        return recu_act_sel(s,f,m,n,act)
         return act
+
 
 s = []
 f = []
-n = len(s)
 start = list(map(int,input().split()))
 finish = list(map(int,input().split()))
+n = len(start)
 
 sortedactivity = sorted(zip(finish,start))
 
 for i in range(n):
-    s = s.append(sortedactivity[i][1])
-    f = f.append(sortedactivity[i][0])
-
-k = 0
-print(recu_act_sel(s,f,k,n))
+    s.append(sortedactivity[i][1])
+    f.append(sortedactivity[i][0])
+act = []
+k = -1
+recu_act_sel(s,f,k,n,act)
+print(act)
+sortedactivity = list(zip(s,f))
+for i in range(len(act)-1):
+    print(sortedactivity[act[i]])
